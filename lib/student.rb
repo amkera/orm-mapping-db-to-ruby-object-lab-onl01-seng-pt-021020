0dd55d6f2c118054f5d7eb9c642844b74c53f4d4
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-14 20:10:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,9 +91,10 @@ class Student
       WHERE grade = 10 
       LIMIT 1
     SQL
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end 
+    
+    first_student_row = 
+    DB[:conn].execute(sql)[0]
+    self.new_from_db(first_student_row)
   end 
       
   
